ecc4308278385c6e2b9d4017a24d64dc85428cfe
Jenson Bruins <jenson@jensonbruins.nl>
2022-04-11 11:29:50 +0200

    feat: new standard ip

--- start_connection.sh
+++ start_connection.sh
@@ -6,5 +6,5 @@ source ./catkin_ws/devel/setup.bash
 path_calibration=`pwd`"/my_robot_calibration.yaml"
 echo "$path_calibration"
 
-roslaunch ur_calibration calibration_correction.launch robot_ip:=192.168.137.2 target_filename:="$path_calibration" &
-sleep 10 && roslaunch ur_robot_driver ur10e_bringup.launch robot_ip:=192.168.137.2 kinematics_config:="$path_calibration"
+roslaunch ur_calibration calibration_correction.launch robot_ip:=10.42.0.2 target_filename:="$path_calibration" &
+sleep 10 && roslaunch ur_robot_driver ur10e_bringup.launch robot_ip:=10.42.0.2 kinematics_config:="$path_calibration"
