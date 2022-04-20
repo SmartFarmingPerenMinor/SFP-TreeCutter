9eebea61b743f7fa54327f41266c79c119daccbf
Anthony Quach <ta.quach@hotmail.com>
2022-04-20 12:10:50 +0200

    Print read

--- catkin_ws/src/modbus/src/main.cpp
+++ catkin_ws/src/modbus/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 	try
 	{
 		modbus::ModbusTalker mbus("10.42.0.2", 50009);
-		mbus.sendMsg("0x030xff0x000x000x05");
+		std::cout << mbus.sendMsg("0x030xff0x000x000x05") << std::endl;
 	}
 	catch(const std::exception& e)
 	{
