5f81a105e48445ed726ba714c42219f503ff7d46
Anthony Quach <ta.quach@hotmail.com>
2022-04-26 15:55:17 +0200

    Fix errors

--- startgazebo.sh
+++ startgazebo.sh
@@ -5,8 +5,6 @@
 source /opt/ros/noetic/setup.bash
 source ./catkin_ws/devel/setup.bash
 
-roslaunch ur_gazebo ur10e_bringup.launch limited:=true &
+roslaunch ur_gazebo ur10e_bringup.launch limited:=false &
 sleep 8
 
-roslaunch ur10e_moveit_config ur10e_moveit_planning_execution.launch sim:=true limited:=true &
-sleep 8
--- startrvizgazebo.sh
+++ startrvizgazebo.sh
@@ -10,5 +10,8 @@ source ./catkin_ws/devel/setup.bash
 sleep 15
 
 
-roslaunch ur10e_moveit_config moveit_rviz.launch config:=true &
+roslaunch ur10e_moveit_config ur10e_moveit_planning_execution.launch sim:=true &
+sleep 8
+
+roslaunch ur10e_moveit_config moveit_rviz.launch &
 sleep 15
