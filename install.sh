1d2bb58e05ef68f6f329c47a2089783158460be1
Dmitri Creutzburg <dmitri.c@live.nl>
2022-05-09 11:34:15 +0200

    add install line to sh script

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ cd ./catkin_ws/src/test/worlds/ && ./setup.sh
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
 sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg -y
+sudo apt-get install ros-noetic-realsense2-camera
 sudo apt-get update && sudo apt-get upgrade -y 
 
 sudo rosdep init && rosdep update
