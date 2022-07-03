4f8b85be7fe24420da5d8abf1631f8f50c858fb8
Antusqus <ta.quach@hotmail.com>
2022-07-03 13:13:07 +0200

    Final

--- startrvizgazebo.sh
+++ startrvizgazebo.sh
@@ -9,14 +9,5 @@ source ./catkin_ws/devel/setup.bash
 ./startgazebo.sh &
 sleep 15
 
-<<<<<<< HEAD
-
-roslaunch ur10e_moveit_config ur10e_moveit_planning_execution.launch sim:=true &
-sleep 8
-
-roslaunch ur10e_moveit_config moveit_rviz.launch &
-=======
-rvizConfig=$(pwd)"/configs/rviz_ur10e_config.rviz"
-roslaunch ur10e_moveit_config moveit_rviz.launch config:=true rviz_config="$rvizConfig" &
->>>>>>> 8f1d052760c4ef8ad572d90f5beec98544983296
+roslaunch ur10e_moveit_config moveit_rviz.launch config:=true &
 sleep 15
